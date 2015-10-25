8b13796dc3aee966fa901aa425001682670a968f
Maxim Korolev <mkorolev@splinex.com>
2015-10-25 23:27:34 +0500

    fix

--- level/camera.py
+++ level/camera.py
@@ -7,9 +7,9 @@ class Camera(object):
     
     def __init__(self, **kwargs):
         self.usePiCamera = kwargs.get('pi_camera', False)
-#         if self.usePiCamera:
-#             import picamera
-#             import picamera.array
+        if self.usePiCamera:
+            import picamera
+            import picamera.array
             
         self.resolutionDiv = int(kwargs.get('resolution_div', 2))
         self.framerate = int(kwargs.get('faramerate', 2))
