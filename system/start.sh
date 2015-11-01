527a6ad367718497fd7ea5f47a3642e89ce524e0
Maxim Korolev <mkorolev@splinex.com>
2015-11-02 01:44:20 +0500

    start loop

--- system/start.sh
+++ system/start.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
 
-PYTHONPATH=$PYTHONPATH:/home/pi/PyContrib:/home/pi/Level python3 /home/pi/Level/level/service.py --config /home/pi/Level/configuration/level-prod.cfg
+while true
+do
+	sleep 5
+	PYTHONPATH=$PYTHONPATH:/home/pi/PyContrib:/home/pi/Level python3 /home/pi/Level/level/service.py --config /home/pi/Level/configuration/level-prod.cfg
+done
